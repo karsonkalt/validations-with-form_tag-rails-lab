71e8e4a183bf9f314bb257ab34d22b9c6489a411
Karson <kars.kalt@gmail.com>
2021-03-25 16:30:09 -0600

    commit tree 50925aa842b233b356dcf32ea0ad71f727fcaebb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 100}
+
+    # can't remember this validator
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+#   1. `title` is not blank
+#   1. `content` is at least 100 characters long
+#   1. `category` is either `"Fiction"` or `"Non-Fiction"`
 end
